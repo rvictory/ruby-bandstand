bcd3c1d517e246bcb388f6b87e8ca7870f9dbd78
Ryan Victory <rvictory@gmail.com>
2013-07-24 19:00:09 -0500

    GPL License

--- piano_bar.rb
+++ piano_bar.rb
@@ -1,3 +1,20 @@
+=begin
+    BandStand - Web interface to pianobar
+    Copyright (C) 2012-2013 Ryan M. Victory
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see [http://www.gnu.org/licenses/].
+=end
 require 'thread'
 class PianoBar
 
@@ -90,7 +107,7 @@ class PianoBar
   end
 
   def self.output
-    @@output
+   @@output
   end
 
   def self.current_song
